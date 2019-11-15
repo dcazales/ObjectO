653db1104f0622bf19242f178660264352095fe7
Diana.Cazales <dianacazales>
2019-11-15 15:38:48 -0600

    Saving as a specific  file type is done.

---
+++ Insurance 4.0/src/InsuranceApp.java
@@ -0,0 +1,166 @@
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+//Presents a welcome message
+
+public class InsuranceApp {
+       	 public static void printStars(int howMany) {
+       	        String stars = "";
+       	        for (int i = 0; i < howMany; i++) {
+       	            stars = stars + "*";
+       	        }
+       	        System.out.println(stars);
+       	    }
+
+       
+       	 
+
+
+       public static void showMenu() {
+       	System.out.println("\n");
+           System.out.println("Here are your choices: \n");
+           System.out.println("\t1.  List Members:");
+           System.out.println("\t2.  Add a new member");
+           System.out.println("\t3.  Save members");
+           System.out.println("\t4.  Load members");
+           System.out.println("\t5.  Assess members:");
+           System.out.println("\t6.  Save assessments as JSON, ");
+           System.out.println("\t7.  Exit");
+           System.out.print("Enter the number of your choice:");
+
+       }
+
+       public static void main(String[] args) throws IOException {
+    	   printStars(58);
+           System.out.print("\t\tINSURANCE SCORE CARD \n This app scores a potential customer on various health \n"
+           		+ "attributes: blood pressure, age, height, weight, and\n" + 
+           		"family history of disease. It writes each member's \n"
+           		+ "insurance grade to a JSON file so that they can be easily\n "
+           		+ "shared on a web based data exchange! \n");
+           printStars(58);
+       
+           
+    	   Scanner sc = new Scanner(System.in);
+    	   Scanner option = new Scanner(System.in);
+           System.out.print("Enter name of data file: ");
+           String filename = sc.nextLine();
+           ArrayList<Member>members = MemberReader.readMembersFromTextFile(filename);
+           
+           
+           int choice, count=0; //for the menu
+          
+           
+           
+       	if (members == null) {
+           	System.out.println("Something bad happened, try again.");
+           }else {
+        	   
+        	   for(Member m: members ) {
+        		   count = count + 1;
+       		}
+        	System.out.printf("%s members read.", count);
+        	   
+        	   
+          
+           	do {
+                   showMenu();
+                   
+                   choice = sc.nextInt();
+                   System.out.println("");
+                  //Choices begin *_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
+                   
+                   if (choice == 1) {
+                   System.out.print("Here are the Memebers: \n");
+                   MemberWriter.writeMembersToScreen(members);
+                   
+                   }else if (choice == 2) {
+                	   String firstName, lastName, c, d, a;
+                   	int age, height, weight, bpSyst, bpDias;    
+                       System.out.println("Enter first name:");
+                       firstName = sc.next().trim();
+                       System.out.println("Enter last name:");
+                       lastName = sc.next().trim();
+                       System.out.println("Enter age:");
+                       age = sc.nextInt();
+                       System.out.println("Enter height in inches:");
+                       height = sc.nextInt();
+                       System.out.println("Enter weight in pounds:");
+                       weight = sc.nextInt();
+                       System.out.println("Enter blood pressure sys:");
+                       bpSyst = sc.nextInt();
+                       System.out.println("Enter blood pressure dia:");
+                       bpDias = sc.nextInt();
+                       System.out.println("Has a family member had...");
+                       System.out.println("Cancer? (y/n):");
+                       c = sc.next().trim();
+                       System.out.println("Diabetes? (y/n):");
+                       d = sc.next().trim();
+                       System.out.println("Alzheimers? (y/n):");
+                       a = sc.next().trim();
+                       
+                        FileWriter fileWriter = new FileWriter(filename, true); //Set true for append mode
+                  	    PrintWriter printWriter = new PrintWriter(fileWriter);
+                  	    printWriter.println(firstName+"	"+lastName+"	"+age+"	"+height+"	"+weight+"	"+bpSyst+"	"+bpDias+"	"+c+"	"+d+"	"+a);  //New line
+                  	    printWriter.close();
+                  	    members.add(new Member(firstName, lastName, age, height, weight, bpSyst, bpDias, c, d, a));
+                      }
+                      else if (choice == 3) {
+                      	System.out.print("(T)ext, (B)inary, or (X)ML?");
+                      	String textOption = option.nextLine();
+                      	System.out.println("Enter then name of the output file: ");
+                      	filename = option.nextLine();
+                      	
+                      	if (textOption.equals("B")) {
+                      		MemberWriter.writeMembersToBinary(filename, members);
+                      	    System.out.println("Members were written successfully.");
+
+                      	} else if (textOption.equals("T")) {
+                      		MemberWriter.writeMembersToTextFile(filename, members);
+                      	    System.out.println("Members were written successfully.");
+
+                      	
+                      	}else if (textOption.equals("X")) {
+                      		MemberWriter.writeMembersToXML(filename, members);
+                      	    System.out.println("Members were written successfully.");
+
+                      	}
+                   	
+                   } 
+                   else if (choice == 4) {
+                   
+                	   MemberReader.readMembersFromTextFile(filename);
+
+                   }
+                   else if (choice == 5) {
+                	   InsuranceScoreWriter.writeAssesmentToScreen(members);
+                      
+                   }
+                   else if (choice == 6) {
+                       
+                    	   
+                	   
+                	   
+                	   
+                       }
+           	}
+           	while (choice != 6);
+           	System.out.println("Have a good day.");
+           }
+      }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Insurance 4.0/src/Member.java
@@ -0,0 +1,121 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+ 
+public class Member implements Serializable {
+    private String firstName,lastName, cancer, diabetes, alzheimers ;
+    private int age, height, weight, bpSyst, bpDias;
+    
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLasttName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getCancer() {
+        return cancer;
+    }
+    public void setCancer(String cancer) {
+        this.cancer = cancer;
+    }
+    public String getDiabetes() {
+        return diabetes;
+    }
+    public void setDiabetes(String diabetes) {
+        this.diabetes = diabetes;
+    }
+    public String getAlzheimers() {
+        return alzheimers;
+    }
+    public void setAlzheimers(String alzheimers) {
+        this.alzheimers = alzheimers;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public int getBpDias() {
+        return bpDias;
+    }
+    public void setBpDias(int bpDias) {
+        this.bpDias = bpDias;
+    }
+    public int getBpSyst() {
+        return bpSyst;
+    }
+    public void setBpSyst(int bpSyst) {
+        this.bpSyst = bpSyst;
+    }
+    
+
+    public Member() {
+    	 firstName ="";
+    	 lastName="";
+         age= 0;
+         height=0;
+         weight=0;
+         bpSyst=0;
+         bpDias=0;
+         cancer= "";
+         diabetes="";
+        alzheimers="";
+    }
+    
+    
+    public Member(String firstName, String lastName, int age, int height, int weight, 
+    	int bpSyst, int bpDias, String cancer, String diabetes, String alzheimers) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAge(age);
+        setHeight(height);
+        setWeight(weight);
+        setBpSyst(bpSyst);
+        setBpDias(bpDias);
+        setCancer(cancer);
+        setDiabetes(diabetes);
+        setAlzheimers(alzheimers);
+        
+        
+   
+    }
+  
+    @Override
+    public String toString() {
+        return String.format(
+        		
+        		"%s, %s \n\n "
+        		
+        		+ "Age:		%d\n"
+        		+ "Height:		%d in\n"
+        		+ "Weight		%d lb\n"
+        		+ "BP Syst:	%d\n"
+        		+ "BP Dias:	%d\n"
+        		+ "Cancer:		%s\n"
+        		+ "Diabetes:	%s\n"
+        		+ "Alzheimers:	%s\n"
+        		+ "-----------------------------"
+        		,lastName, firstName,age, height,weight,bpSyst, bpDias, cancer, diabetes, alzheimers);
+        		}
+        	
+    
+}
