7484befcafa49e6bb4e757b067bf3bcd481be9ab
Diana.Cazales <dianacazales>
2019-11-18 00:10:38 -0600

    Finished version, however, there are somethings that I do not like. For option 3 it will print out the version of the score and verdict. Instead of printing the tab file. Thats the only bug I Would want to fix. Therefore, I wont submit till tomorrow.

---
+++ Insurance finished/src/InsuranceScoreWriter.java
@@ -0,0 +1,47 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+//write the insurance risk information in a nicely formatted 
+//way to the screen
+// write the insurance risk information to a JSON file.
+public class InsuranceScoreWriter {
+	
+	public static void writeMembers(ArrayList<InsuranceScore> insuranceScores) {	       
+		            for (InsuranceScore is : insuranceScores) {
+		            	System.out.println(is+"\n");   
+		    } 
+	
+	
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
